8fc3c7910c1a0d1a3fbaefffde27b022da33fd34
teastainGit <80008504+teastainGit@users.noreply.github.com>
2022-08-05 01:36:47 -0400

    Speling and add timers, one decimal place printout

--- PaHub2ENV/PaHub2ENV.ino
+++ PaHub2ENV/PaHub2ENV.ino
@@ -1,6 +1,6 @@
 #include <M5Stack.h> //use any controller by substituting the library
-#include "SHT3X.h"   // the temp sensor (also the hunidity sensor)
-SHT3X sht30;         // reates ONE instance!!!
+#include "SHT3X.h"   // the temp sensor (also the humidity sensor)
+SHT3X sht30;         // creates ONE instance!!!
 float temp1;         // value on ENV1
 float temp2;         // value on ENV2
 void setup() {
@@ -19,19 +19,21 @@ void loop() {
   if (sht30.get() == 0) {      //reads the ENV at port 0
     temp1 = sht30.cTemp;       //assigns the reading to "temp1"
   }
+  delay(100);
   Wire.beginTransmission(0x70);//(the PaHub)
   Wire.write(0x02);            //reads the ENV at port 1
   Wire.endTransmission();
   if (sht30.get() == 0) {
     temp2 = sht30.cTemp;        //assigns the reading to "temp2"
   }
+  delay(100);
   M5.Lcd.fillRect(0, 0, 320, 80, BLACK);
   M5.Lcd.setCursor(5, 5);
   M5.Lcd.print("Temp1=  ");
   M5.Lcd.setCursor(130, 5);
-  M5.Lcd.print(temp1);
+  M5.Lcd.print(temp1,1);        //one decimal place
   M5.Lcd.setCursor(5, 50);
   M5.Lcd.print("Temp2=  ");
   M5.Lcd.setCursor(130, 50);
-  M5.Lcd.print(temp2);
+  M5.Lcd.print(temp2,1);        //one decimal place
 }
